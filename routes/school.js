75588e5f588abba67a3e31a9aa92d9b2d4a3e015
junwkdd <junwkdd@gmail.com>
2019-12-29 15:38:43 +0900

    options -> get

--- routes/school.js
+++ routes/school.js
@@ -3,7 +3,23 @@ var router = express.Router();
 
 var schoolInfo = require('../model/schoolInfo');
 
-router.options('/meal', function(req, res, next) {
+// router.options('/meal', function(req, res, next) {
+//   const year = String(req.query.year);
+//   const month = String(req.query.month);
+//   schoolInfo.getMeal(year, month, function(meal) {
+//     res.json(meal);
+//   });
+// });
+
+// router.options('/calendar', function(req, res, next) {
+//   const year = String(req.query.year);
+//   const month = String(req.query.month);
+//   schoolInfo.getCalendar(year, month, function(calendar) {
+//     res.json(calendar);
+//   });
+// });
+
+router.get('/meal', function(req, res, next) {
   const year = String(req.query.year);
   const month = String(req.query.month);
   schoolInfo.getMeal(year, month, function(meal) {
@@ -11,7 +27,7 @@ router.options('/meal', function(req, res, next) {
   });
 });
 
-router.options('/calendar', function(req, res, next) {
+router.get('/calendar', function(req, res, next) {
   const year = String(req.query.year);
   const month = String(req.query.month);
   schoolInfo.getCalendar(year, month, function(calendar) {
